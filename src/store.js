6fca5a1aab5846b5c483b2806726ac5b1626c89d
Young <navskh@naver.com>
2023-04-02 20:24:29 +0900

    update

--- src/store.js
+++ src/store.js
@@ -1,31 +1,29 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 let user = createSlice({
-  name: 'user',
-  initialState: 'kim',
-  reducer: {
-    changeName(state) {
-      state.name = 'john ' + state;
-    }
-  }
-})
-
+	name: 'user',
+	initialState: 'kim',
+	reducers: {
+		changeName(state) {
+			return 'john ' + state;
+		},
+	},
+});
 
 let stock = createSlice({
-  name: 'stock',
-  initialState: [
-    {id : 0, name : 'White and Black', count : 2},
-    {id : 2, name : 'Grey Yordan', count : 1}
-  ]
-  ,
-})
+	name: 'stock',
+	initialState: [
+		{ id: 0, name: 'White and Black', count: 2 },
+		{ id: 2, name: 'Grey Yordan', count: 1 },
+	],
+});
 
 export const { changeName } = user.actions;
 
 export default configureStore({
 	reducer: {
-    user: user.reducer,
-    stock: stock.reducer
+		user: user.reducer,
+		stock: stock.reducer,
 	},
 }); 
 
